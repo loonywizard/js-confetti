d090fac12fc69f3e3a900a69da7736cd574b7180
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2021-02-27 14:24:04 +0300

    Refactor ConfettiShape with ISpeed and IRadius types

--- src/ConfettiShape.ts
+++ src/ConfettiShape.ts
@@ -1,15 +1,14 @@
-import { IPosition } from './types'
+import { IPosition, ISpeed, IRadius } from './types'
 import { generateRandomNumber } from './generateRandomNumber'
 import { generateRandomRGBColor } from './generateRandomRGBColor'
 
 
 class ConfettiShape {
-  speedX: number
-  speedY: number
+  speed: ISpeed
   rotationSpeed: number
 
-  radiusX: number
-  radiusY: number
+  radius: IRadius
+
   angle: number
   position: IPosition
   color: string
@@ -17,21 +16,28 @@ class ConfettiShape {
   radiusYUpdateDirection: 'up' | 'down'
 
   constructor(position: IPosition, direction: 'left' | 'right') {
-    this.speedX = 0.1 * generateRandomNumber(500, 900) / 100
-    this.speedY = 0.1 * generateRandomNumber(500, 900) / 100
+    this.speed = {
+      x: 0.1 * generateRandomNumber(500, 900) / 100,
+      y: 0.1 * generateRandomNumber(500, 900) / 100,
+    }
+
     this.rotationSpeed = 0.1 * generateRandomNumber(20, 1000) / 100
-    this.radiusX = 10
-    this.radiusY = 10
+
+    this.radius = {
+      x: 10, y: 10
+    }
+
     this.radiusYUpdateDirection = 'down'
     this.angle = direction === 'left' 
       ? generateRandomNumber(-140, 0) * Math.PI / 180
       : generateRandomNumber(0, -140) * Math.PI / 180
+      
     this.position = { ...position }
     this.color = generateRandomRGBColor()
   }
 
   draw(canvasContext: CanvasRenderingContext2D): void {
-    const { position, radiusX, radiusY, color } = this
+    const { position, radius, color } = this
     const dpr = window.devicePixelRatio
 
     canvasContext.fillStyle = color
@@ -39,33 +45,33 @@ class ConfettiShape {
     canvasContext.beginPath()
 
     canvasContext.ellipse(
-      position.x * dpr, position.y * dpr, radiusX * dpr, radiusY * dpr,
+      position.x * dpr, position.y * dpr, radius.x * dpr, radius.y * dpr,
       0, 0, 2 * Math.PI,
     )
     canvasContext.fill()
   }
 
   updatePosition(timeDelta: number): void {
-    const { speedX, speedY, angle, radiusYUpdateDirection, rotationSpeed } = this
+    const { speed, angle, radiusYUpdateDirection, rotationSpeed } = this
     
-    this.position.x += Math.cos(angle) * timeDelta * speedX
-    this.position.y += Math.sin(angle) * timeDelta * speedY
+    this.position.x += Math.cos(angle) * timeDelta * speed.x
+    this.position.y += Math.sin(angle) * timeDelta * speed.y
 
     if (radiusYUpdateDirection === 'down') {
-      this.radiusY -= timeDelta * rotationSpeed * 0.1
-      if (this.radiusY <= 0) {
-        this.radiusY = 0
+      this.radius.y -= timeDelta * rotationSpeed * 0.1
+      if (this.radius.y <= 0) {
+        this.radius.y = 0
         this.radiusYUpdateDirection = 'up'
       }
     } else {
-      this.radiusY += timeDelta * rotationSpeed * 0.1
-      if (this.radiusY >= 10) {
-        this.radiusY = 10
+      this.radius.y += timeDelta * rotationSpeed * 0.1
+      if (this.radius.y >= 10) {
+        this.radius.y = 10
         this.radiusYUpdateDirection = 'down'
       }
     }
 
-    this.speedY -= 0.0005 * timeDelta
+    this.speed.y -= 0.0005 * timeDelta
   }
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,12 @@
-interface IPosition {
+interface I2DCoordinates {
   x: number,
   y: number,
 }
 
-export { IPosition }
+type IPosition = I2DCoordinates
+
+type ISpeed = I2DCoordinates
+
+type IRadius = I2DCoordinates
+
+export { IPosition, ISpeed, IRadius }
