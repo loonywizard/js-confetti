e56b946f2c6649da5a968352689eea3180d6a71c
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2021-02-28 15:09:24 +0300

    Set webpackConfig.output.globalObject to this

--- webpack.config.ts
+++ webpack.config.ts
@@ -14,6 +14,7 @@ const config: Configuration = {
     path: path.resolve(__dirname, 'dist'),
     library: 'jsConfetti',
     libraryTarget: 'umd',
+    globalObject: 'this',
   },
   module: {
     rules: [
