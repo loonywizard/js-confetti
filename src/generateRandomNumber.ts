e195abffca9b1bf304a35d51a654efc097e6aac3
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2021-02-28 13:28:31 +0300

    Add support for fractionDigits param in generateRandomNumber

--- src/generateRandomNumber.ts
+++ src/generateRandomNumber.ts
@@ -1,5 +1,7 @@
-function generateRandomNumber(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1) ) + min
+function generateRandomNumber(min: number, max: number, fractionDigits = 0): number {
+  const randomNumber = Math.random() * (max - min + 1) + min
+
+  return Math.floor(randomNumber * 10 ** fractionDigits) / (10 ** fractionDigits)
 }
 
 export { generateRandomNumber }
