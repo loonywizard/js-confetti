5c2e58a87ee48ff3982a444737361d929a105589
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2021-02-28 23:33:31 +0300

    Use rollup for library build

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import typescript from 'rollup-plugin-typescript2'
+import babel from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+
+export default [
+  // ES
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/es/index.js', format: 'es',
+    },
+    plugins: [
+      typescript(),
+      babel({
+        extensions: ['.ts'],
+      }),
+    ]
+  },
+
+  // UMD
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/js-confetti.min.js',
+      format: 'umd',
+      name: 'jsConfetti',
+      indent: false,
+    },
+    plugins: [
+      typescript(),
+      babel({
+        extensions: ['.ts'],
+        exclude: 'node_modules/**',
+      }),
+      terser(),
+    ],
+  },
+]
